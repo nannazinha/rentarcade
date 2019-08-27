77780d134ed90fdaf49bc76b2e99cbbd2367ed43
natalia torres <nannazinha@gmail.com>
2019-08-27 10:01:04 -0300

    add owner panel

---
+++ app/javascript/components/h1.js
@@ -0,0 +1,9 @@
+import Typed from 'typed.js';
+const initTyped = () => {
+  new Typed('#typed', {
+    strings: ["PAINEL DO PROPRIETARIO_"],
+    typeSpeed: 100,
+    loop: false,
+  });
+}
+export { initTyped };
