2eaa7b336ce603eacb74ba5eab1e64ff61b85747
mirian pacheco <mirian.alvesp@gmail.com>
2019-08-28 17:22:14 -0300

    calendar works

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,5 +1,4 @@
 import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 flatpickr(".datepicker", {})
