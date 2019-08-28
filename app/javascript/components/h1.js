ca9070e52a9708e76d6fb83b9dd05351c26c1b5e
natalia torres <nannazinha@gmail.com>
2019-08-28 13:40:05 -0300

    transactions index & other things

--- app/javascript/components/h1.js
+++ app/javascript/components/h1.js
@@ -2,13 +2,13 @@ import Typed from 'typed.js';
 const initTyped = () => {
   new Typed('#typed', {
     strings: ["PAINEL DO PROPRIETARIO_"],
-    typeSpeed: 100,
+    typeSpeed: 50,
     loop: false,
   });
 
-  new Typed('#typed_player', {
-    strings: ["PAINEL DO PLAYER_"],
-    typeSpeed: 100,
+  new Typed('#typed-transactions-index', {
+    strings: ["RESERVAS_"],
+    typeSpeed: 50,
     loop: false,
   });
 }
