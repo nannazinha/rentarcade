3b54e3df324b95bdf9ef6ed04c8251e7d99de038
Thierry Oliveira <thierry_fernando@hotmail.com>
2019-08-29 17:25:50 -0300

    Fixe table and style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,10 @@
 import "bootstrap";
-import { initTyped } from '../components/h1'
+import { initTyped } from '../components/h1_reserva.js'
+import { initTyped2 } from '../components/h1_anuncio.js'
 import "../plugins/flatpickr"
 
 // import priceCalc from '../components/pricecalc';
 // priceCalc();
 
 initTyped();
+initTyped2();
