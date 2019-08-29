552b28c5de6d952d481af51e479de92a81172be6
Thierry Oliveira <thierry_fernando@hotmail.com>
2019-08-29 15:49:55 -0300

    Fixed some, and broke some css

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
+  has_many :transactions
 end
