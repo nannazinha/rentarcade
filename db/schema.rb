a9356828d8eaf3953e021e774f20384b27571333
Thierry Oliveira <thierry_fernando@hotmail.com>
2019-08-29 21:02:19 -0300

    Add style do devise links and forms

--- db/schema.rb
+++ db/schema.rb
@@ -15,22 +15,6 @@ ActiveRecord::Schema.define(version: 2019_08_29_194447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cart_items", force: :cascade do |t|
-    t.bigint "cart_id"
-    t.bigint "item_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
-    t.index ["item_id"], name: "index_cart_items_on_item_id"
-  end
-
-  create_table "carts", force: :cascade do |t|
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_carts_on_user_id"
-  end
-
   create_table "items", force: :cascade do |t|
     t.bigint "user_id"
     t.string "console_model"
@@ -79,9 +63,6 @@ ActiveRecord::Schema.define(version: 2019_08_29_194447) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "cart_items", "carts"
-  add_foreign_key "cart_items", "items"
-  add_foreign_key "carts", "users"
   add_foreign_key "items", "users"
   add_foreign_key "reviews", "items"
   add_foreign_key "reviews", "users"
