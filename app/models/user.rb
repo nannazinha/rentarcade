21561accb9da095d073e4671d9adaf19cd79b6bc
Thierry Oliveira <thierry_fernando@hotmail.com>
2019-08-30 14:44:38 -0300

    Create profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items
 
-  has_many :reviews
+  has_many :reviews, through: :items
 
   has_many :transactions
 end
