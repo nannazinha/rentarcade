5cf80b17325ddadb654bd09a5bfefc830f5e49b4
natalia torres <nannazinha@gmail.com>
2019-08-30 15:08:48 -0300

    reviews with stars

--- config/application.rb
+++ config/application.rb
@@ -16,12 +16,10 @@ module Rentarcade
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
+     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
 
-  config.i18n.default_locale = :'pt-BR'
-  config.i18n.available_locales = :'pt-BR'
 end
